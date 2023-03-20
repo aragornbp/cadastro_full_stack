56921da53b55408cc02b1097963ee4bf1bffbc95
Bruno Paulin <bruno.p-@hotmail.com>
2023-03-20 14:41:14 -0300

    feat: add entities and migrations

---
+++ backend/src/entities/contact.entity.ts
@@ -0,0 +1,25 @@
+import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import {Client} from "./client.entity";
+
+@Entity('contact')
+export class Contact{
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column({length:127, unique: true})
+  email: string;
+
+  @Column({length: 127})
+  name: string;
+
+  @Column({unique: true})
+  phone: string
+
+  @CreateDateColumn()
+  created_at: Date
+
+  @ManyToOne(()=> Client, {onDelete: "CASCADE"})
+  client: Client
+
+  
+}
