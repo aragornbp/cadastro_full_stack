b706929da4bbed9bc06a01a2cdcf5ec80728c7d1
Bruno Paulin <bruno.p-@hotmail.com>
2023-03-22 15:37:49 -0300

    fix backend

--- backend/dist/repositories/clientRepository.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.clientRepository = void 0;
-const data_source_1 = require("../data-source");
-const client_entity_1 = require("../entities/client.entity");
-exports.clientRepository = data_source_1.AppDataSource.getRepository(client_entity_1.Client);
--- backend/dist/repositories/contactRepository.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.contactRepository = void 0;
-const data_source_1 = require("../data-source");
-const contact_entity_1 = require("../entities/contact.entity");
-exports.contactRepository = data_source_1.AppDataSource.getRepository(contact_entity_1.Contact);
